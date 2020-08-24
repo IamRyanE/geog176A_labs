676b69e7b956826d2514f372b4363444b6a5a565
IamRyanE <ryanerickson407@gmail.com>
2020-08-24 13:40:18 -0700

    test

---
+++ installed_libraries.R
@@ -0,0 +1,13 @@
+#Libraries
+
+install.packages("rgdal")
+install.packages("ggthemes")
+install.packages("kableExtra")
+
+
+remotes::install_github("ropensci/USAboundaries")
+remotes::install_github("ropensci/USAboundariesData")
+remotes::install_github("ropenscilabs/rnaturalearth")
+remotes::install_github("ropenscilabs/rnaturalearthdata")
+
+
